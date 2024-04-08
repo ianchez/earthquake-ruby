5127964205c6fab79bc40a98650f8669604cb546
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-08 23:19:43 +0200

    chore: WIP - features controller: GET returns formatted data

--- backend/app/controllers/api/features_controller.rb
+++ backend/app/controllers/api/features_controller.rb
@@ -1,8 +1,33 @@
 module Api
   class FeaturesController < ApplicationController
     def index
-    @features = Feature.all
-    render json: @features
+      formatted_features = Feature.all.map do |feature|
+        {
+          id: feature.id,
+          type: "feature",
+          attributes: {
+            external_id: feature.external_id,
+            magnitude: feature.mag,
+            place: feature.place,
+            time: feature.time,
+            # Converting tsunami to boolean: 0 is false, 1 is true
+            tsunami: feature.tsunami.to_i == 1,
+            mag_type: feature.mag_type,
+            title: feature.title,
+            coordinates: {
+              longitude: feature.longitude,
+              latitude: feature.latitude
+            }
+          },
+          links: {
+            external_url: feature.url
+          }
+        }
+      end
+
+      render json: {
+        data: formatted_features,
+      }
     end
   end
 end
