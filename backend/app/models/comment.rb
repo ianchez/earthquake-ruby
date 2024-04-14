98f1eb9c7f11c8c072c6d69425641aa1d60e27bf
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-14 13:46:54 +0200

    chore: updated comment model - create method

--- backend/app/models/comment.rb
+++ backend/app/models/comment.rb
@@ -2,4 +2,17 @@ class Comment < ApplicationRecord
   belongs_to :feature
 
   validates :body, presence: true
+
+  def self.validate_and_create(data)
+    feature = Feature.find_by(id: data[:feature_id])
+    return { error: 'Feature not found' } unless feature
+
+    comment = self.new(body: data[:body], feature: feature)
+    if comment.valid?
+      comment.save
+      { success: 'Comment created successfully', comment: comment }
+    else
+      { error: comment.errors.full_messages }
+    end
+  end
 end
