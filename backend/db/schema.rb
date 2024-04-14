93701d80974582b18f6f7ad6d676c91e354d4e75
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-14 13:17:42 +0200

    chore: updated schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_08_205859) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_14_110707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "feature_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["feature_id"], name: "index_comments_on_feature_id"
+  end
 
   create_table "features", id: :serial, force: :cascade do |t|
     t.string "external_id", null: false
@@ -29,4 +37,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_08_205859) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "features"
 end
