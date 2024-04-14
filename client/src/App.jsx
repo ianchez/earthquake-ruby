64f154fdfc3e80b20743c3b259a9a0de69c9988a
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-14 16:26:26 +0200

    refactor: unify css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,21 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> Latest Earthquakes </h1>
+      <h1> Latest Earthquakes </h1>
 
-        <p>
-          Welcome to the app where you can find the latest earthquakes, 
-          and add comments to them for future reference
-        </p>
-        <p>
-          The data gets updated from <a
-            href="https://earthquake.usgs.gov/earthquakes/feed/v1.0/"
-            target='_blank'
-            rel="noreferrer"
-          >
-            USGS Latest 30 days Feed
-          </a> every day, so expect 1 day delay between the feed and this app data
-        </p>
-      </header>
+      <p>
+        Welcome to the app where you can find the latest earthquakes, 
+        and add comments to them for future reference
+      </p>
+      <p>
+        The data gets updated from <a
+          href="https://earthquake.usgs.gov/earthquakes/feed/v1.0/"
+          target='_blank'
+          rel="noreferrer"
+        >
+          USGS Latest 30 days Feed
+        </a> every day, so expect 1 day delay between the feed and this app data
+      </p>
     </div>
   );
 }
