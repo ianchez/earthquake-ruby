ac3df8a79ef3f08e5131cf1eb370274fa193c450
Seba Sanchez <20444043+ianchez@users.noreply.github.com>
2024-04-14 18:53:37 +0200

    chore: allow configuration of mag_type filter in frontend

--- client/src/hooks/useEarthquakes.js
+++ client/src/hooks/useEarthquakes.js
@@ -1,28 +1,30 @@
 import { useState, useEffect } from 'react';
 import ApiService from '../services/ApiService';
 
-const DEFAULT_PAGINATION = {
+export const DEFAULT_VALUES = {
+  magType: [ 'md', 'ml', 'ms', 'mw', 'me', 'mi', 'mb', 'mlg' ],
   page: 1,
   perPage: 10
 };
 
 const useEarthquakes = ({
-  page = DEFAULT_PAGINATION.page,
-  perPage = DEFAULT_PAGINATION.perPage
+  magType = DEFAULT_VALUES.magType,
+  page = DEFAULT_VALUES.page,
+  perPage = DEFAULT_VALUES.perPage
 }) => {
   const [earthquakes, setEarthquakes] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const fetchEarthquakes = async (page, perPage) => {
+  const fetchEarthquakes = async (magType, page, perPage) => {
     setLoading(true);
-    const data = await ApiService.fetchEarthquakes(page, perPage);
+    const data = await ApiService.fetchEarthquakes(magType, page, perPage);
     setEarthquakes(data);
     setLoading(false);
   };
 
   useEffect(() => {
-    fetchEarthquakes(page, perPage);
-  }, [page, perPage]);
+    fetchEarthquakes(magType, page, perPage);
+  }, [magType, page, perPage]);
 
   return { earthquakes, loading };
 }
--- client/src/services/ApiService.js
+++ client/src/services/ApiService.js
@@ -1,7 +1,8 @@
 class ApiService {
-  static async fetchEarthquakes(page, perPage) {
+  static async fetchEarthquakes(mag_type, page, per_page) {
     try {
-      const response = await fetch(`http://localhost:3000/api/features?page=${page}&per_page=${perPage}`);
+      const params = new URLSearchParams({ mag_type, page, per_page });
+      const response = await fetch(`http://localhost:3000/api/features?${params}`);
       const { data } = await response.json();
       return data;
     } catch (error) {
